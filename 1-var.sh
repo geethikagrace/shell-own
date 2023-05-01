3e62cc61e24dcef9809fe3ebc9ca04918f94a68d
geethikagrace <geethika2219@gmail.com>
2023-05-01 00:43:26 -0700

    shellown

--- 1-var.sh
+++ 1-var.sh
@@ -1,9 +1,23 @@
 echo "helloo world"
 
+person1=blessy
+person2=sony
+
+echo "${person1}-hi ${person2}
+ ${person2} -hi ${person1},how r you
+ ${person1}-ela unnav ${person2}
+ ${person2}- nenu bagunna nuv ela unnav ${person1}"
+
+
+###passing aurguments /parameters
+
 person1=$1
 person2=$2
 
 echo "${person1}-hi ${person2}
- ${person2} -hi ${person1},how r
+ ${person2} -hi ${person1},how r you
  ${person1}-ela unnav ${person2}
  ${person2}- nenu bagunna nuv ela unnav ${person1}"
+
+ DATE=$(date)
+  echo "script started executing at : ${DATE}"
