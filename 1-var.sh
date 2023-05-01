584f88b2792b2c72f51bc53547691dc2d7d85bcf
geethikagrace <geethika2219@gmail.com>
2023-05-01 00:02:44 -0700

    shellown

--- 1-var.sh
+++ 1-var.sh
@@ -1,8 +1,9 @@
 echo "helloo world"
 
-person1=sony
-person2=kittu
-echo "kittu-hi sony
- sony -hi kittu,how r
- kittu-ela unnav sony
- sony- nenu bagunna nuv ela unnav kittu"
+person1=blessy
+person2=sony
+
+echo "${person1}-hi ${person2}
+ ${person2} -hi ${person1},how r
+ ${person1}-ela unnav ${person2}
+ ${person2}- nenu bagunna nuv ela unnav ${person1}"
