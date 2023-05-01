1ffaa30c962c8813157abd9caf9f60b0cdd56048
geethikagrace <geethika2219@gmail.com>
2023-05-01 00:59:17 -0700

    shellown

--- 1-var.sh
+++ 1-var.sh
@@ -19,5 +19,17 @@ echo "${person1}-hi ${person2}
  ${person1}-ela unnav ${person2}
  ${person2}- nenu bagunna nuv ela unnav ${person1}"
 
+ ####Requirment:
+ #I need to execute the command in shell script and take the output into variables:**
+
  DATE=$(date)
   echo "script started executing at : ${DATE}"
+
+###Requirment:
+
+##I want to connect data base from shell script , so i need username and password but these r sensitive and should not be exposed.
+
+###**SOL: PROMPTING TO THE USER
+
+echo -n "enter username:"
+read USERNAME
